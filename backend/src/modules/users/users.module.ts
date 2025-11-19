305cd1e53dde3fbf469c533ad5a57e6c8ed070cd
ganciaux <ghislain.anciaux@live.fr>
2025-11-19 13:28:54 +0100

    fix(users): import EnumTypesModule in UsersModule

--- backend/src/modules/users/users.module.ts
+++ backend/src/modules/users/users.module.ts
@@ -4,10 +4,12 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
 import { EnumType } from '../enum-types/entities/enum-type.entity';
+import { EnumTypesModule } from '../enum-types/enum-types.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User, EnumType]),
+    EnumTypesModule
   ],
   controllers: [UsersController],
   providers: [UsersService],
