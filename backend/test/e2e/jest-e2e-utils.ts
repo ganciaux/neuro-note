03a7a208c3ca0cc03a5467dd83ea1ca86bc02792
ganciaux <ghislain.anciaux@live.fr>
2025-11-28 15:05:52 +0100

    test(e2e): add request helpers for GET/POST/PATCH/DELETE and adminUser and token

--- backend/test/e2e/jest-e2e-utils.ts
+++ backend/test/e2e/jest-e2e-utils.ts
@@ -1,9 +1,14 @@
 import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppTestModule } from './app-test.module';
+import { UserFactory } from '../../src/common/factories/user.factory';
+import { USER_ROLES } from '../../src/common/factories/enum-values';
 
 let app: INestApplication;
 export const API_PREFIX = process.env.API_PREFIX || '/api/v1';
+
+export const adminUser = UserFactory.makeEntity({ roleCode: USER_ROLES.ADMIN });
+export const adminToken = 'Bearer MOCK_ADMIN_JWT_TOKEN';
 
 export const getApp = async (): Promise<INestApplication> => {
   if (!app) {
